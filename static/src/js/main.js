9b207df64916f0ca9a8197be135b4a4d95e56e67
akshara2207 <117832952+akshara2207@users.noreply.github.com>
2024-05-22 12:22:10 +0530

    Add files via upload

---
+++ static/src/js/main.js
@@ -0,0 +1,113 @@
+(function () {
+  const win = window;
+  const doc = document.documentElement;
+
+  doc.classList.remove("no-js");
+  doc.classList.add("js");
+
+  // Reveal animations
+  if (document.body.classList.contains("has-animations")) {
+    /* global ScrollReveal */
+    const sr = (window.sr = ScrollReveal());
+
+    sr.reveal(".feature, .pricing-table-inner", {
+      duration: 600,
+      distance: "20px",
+      easing: "cubic-bezier(0.5, -0.01, 0, 1.005)",
+      origin: "bottom",
+      interval: 100,
+    });
+
+    doc.classList.add("anime-ready");
+    /* global anime */
+    anime
+      .timeline({
+        targets: ".hero-figure-box-05",
+      })
+      .add({
+        duration: 400,
+        easing: "easeInOutExpo",
+        scaleX: [0.05, 0.05],
+        scaleY: [0, 1],
+        perspective: "500px",
+        delay: anime.random(0, 400),
+      })
+      .add({
+        duration: 400,
+        easing: "easeInOutExpo",
+        scaleX: 1,
+      })
+      .add({
+        duration: 800,
+        rotateY: "-15deg",
+        rotateX: "8deg",
+        rotateZ: "-1deg",
+      });
+
+    anime
+      .timeline({
+        targets: ".hero-figure-box-06, .hero-figure-box-07",
+      })
+      .add({
+        duration: 400,
+        easing: "easeInOutExpo",
+        scaleX: [0.05, 0.05],
+        scaleY: [0, 1],
+        perspective: "500px",
+        delay: anime.random(0, 400),
+      })
+      .add({
+        duration: 400,
+        easing: "easeInOutExpo",
+        scaleX: 1,
+      })
+      .add({
+        duration: 800,
+        rotateZ: "20deg",
+      });
+
+    anime({
+      targets:
+        ".hero-figure-box-01, .hero-figure-box-02, .hero-figure-box-03, .hero-figure-box-04, .hero-figure-box-08, .hero-figure-box-09, .hero-figure-box-10",
+      duration: anime.random(600, 800),
+      delay: anime.random(600, 800),
+      rotate: [
+        anime.random(-360, 360),
+        function (el) {
+          return el.getAttribute("data-rotation");
+        },
+      ],
+      scale: [0.7, 1],
+      opacity: [0, 1],
+      easing: "easeInOutExpo",
+    });
+  }
+
+  micDiv = document.getElementById("micDiv");
+  confirmDiv = document.getElementById("confirmDiv");
+  generateBtn = document.getElementById("generateBtn");
+
+  let oldContent = generateBtn.innerHTML;
+  generateBtn.addEventListener("click", () => {
+    generateBtn.innerHTML = "Listening...";
+    generateBtn.disabled = true;
+
+    fetch("/get_speech_text/")
+      .then((response) => response.json())
+      .then((data) => {
+        generateBtn.innerHTML = oldContent;
+        generateBtn.disabled = false;
+        if (data.text === "") {
+          return;
+        }
+        document.getElementById("speechText").textContent = data.text;
+        micDiv.classList.toggle("d-none");
+        confirmDiv.classList.toggle("d-none");
+      });
+  });
+
+  document.getElementById("confirmForm").addEventListener("reset", (e) => {
+    micDiv.classList.toggle("d-none");
+    confirmDiv.classList.toggle("d-none");
+  });
+})();
